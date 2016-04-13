528b2708c3d9133c0b107525b12cdadd7234dc34
Joey Hayes <bjoeyhayes@gmail.com>
2016-04-13 18:52:14 -0400

    Plugin meta added and files required

---
+++ simply-sleek-related-posts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Simply Sleek Related Posts
+ *
+ * @package   SSRP
+ * @author    Joey Hayes
+ * @copyright 2016 Joey Hayes
+ * @license   GPL-3.0+
+ * @link      https://github.com/joeyred/simply-sleek-related-posts
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simply Sleek Related Posts
+ * Plugin URI:       	https://github.com/joeyred/simply-sleek-related-posts
+ * Description:      	A simple, yet sleek and stylish plugin for adding related posts to a page as a widget.
+ * Version:          	0.0.1
+ * Author:           	Joey Hayes
+ * Author URI:       	https://github.com/joeyred
+ * Text Domain:      	simply-sleek-related-posts
+ * License:          	GPL-3.0+
+ * License URI:      	http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/joeyred/simply-sleek-related-posts
+ */
+
+require( 'inc/functions.php' );
+require( 'inc/admin.php' );
+require( 'inc/widget.php' );
