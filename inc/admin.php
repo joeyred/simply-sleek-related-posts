cc92d112f2d1359dc1af2bc40ed641745eb4b3ea
Joey Hayes <bjoeyhayes@gmail.com>
2016-04-13 21:32:55 -0400

    Updated for new way of dealing with `$instance`.

--- inc/admin.php
+++ inc/admin.php
@@ -22,39 +22,45 @@ function ssrp_widget_options_form( $instance, $object ) {
   // Get all registered post types
   $post_types = get_post_types( '', 'names' );
 
-  // Check Values
-  if ( $instance ) {
-
-    $title 	   = esc_attr($instance['title']);
-    $num_posts = esc_attr($instance['num_posts']);
-    $post_type = esc_attr($instance['post_type']);
-
-  } else {
-
-    $title     = '';
-    $num_posts = '';
-    $post_type = '';
-  }
+  // // Check Values
+  // if ( $instance ) {
+  //
+  //   $title 	   = esc_attr($instance['title']);
+  //   $num_posts = esc_attr($instance['num_posts']);
+  //   $post_type = esc_attr($instance['post_type']);
+  //
+  // } else {
+  //
+  //   $title     = '';
+  //   $num_posts = '';
+  //   $post_type = '';
+  // }
 
   // Array of form fields in order of output.
   $fields = array(
     'title' => ssrp_make_field( array(
       'label'      => 'Title',
       'field_type' => 'text',
-      'name'       => $object->get_field_name('post_type'),
-      'value'      => $title,
+      'name'       => $object->get_field_name('title'),
+      'value'      => $instance['title'],
     )),
     'post-type' => ssrp_make_field( array(
       'label'      => 'Post Type',
       'field_type' => 'select',
       'name'       => $object->get_field_name('post_type'),
-      'value'      => ssrp_option_loop( $post_types, $post_type, 'foreach' ),
+      'value'      => ssrp_option_loop( $post_types, $instance['post_type'], 'foreach' ),
     )),
     'number-of-posts' => ssrp_make_field( array(
       'label'      => 'Number of Posts Displayed',
       'field_type' => 'select',
       'name'       => $object->get_field_name('num_posts'),
-      'value'      => ssrp_option_loop( 6, $num_posts, 'for' ),
+      'value'      => ssrp_option_loop( 6, $instance['num_posts'], 'for' ),
+    )),
+    'test-for-loop-selection' => ssrp_make_field( array(
+      'label'      => 'Test the For Loop',
+      'field_type' => 'select',
+      'name'       => 'testing',
+      'value'      => ssrp_option_loop( 6, 3, 'for' ),
     )),
   );
 
